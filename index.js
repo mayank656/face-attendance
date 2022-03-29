fe0cfe6f0d3e98a3ee39f679cd002a2d294965c9
ToymanShikhar <shikharverma043@gmail.com>
2022-03-29 22:33:44 +0530

    register page

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
 const {spawn} = require('child_process');
 
 const app = express();
@@ -12,6 +17,19 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+app.use(
+    session({
+        secret: "Our little secret.",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const saltRounds = 10;
+
 const id = "admin-shikhar";
 const password = process.env.PASSWORD;
 const database_url = "mongodb+srv://" + id + ":" + password + "@cluster0.gxjfw.mongodb.net/attendance";
@@ -19,12 +37,32 @@ mongoose.connect(database_url, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 });
+
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String,
-    image: String
+    name: String,
+    address: String,
+    mobile: Number,
+    age: Number
 });
+
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
 
 app.get("/", function(req, res){
     res.render("dashboard");
@@ -34,27 +72,40 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-    let userName = req.body.username;
-    let passWord = req.body.password;
+app.post("/register", function (req, res) {
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', ['register.py', req.body.userid]);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+     console.log('Pipe data from python script ...');
+     dataToSend = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    });
 
-    User.findOne({username: userName}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        } else {
-            if(foundUser){
-                res.send("User already registered");
+    User.register(
+        {
+            username: req.body.username,
+            name: req.body.name,
+            address: req.body.address,
+            mobile: req.body.mobile,
+            age: req.body.age
+        },
+        req.body.password,
+        function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
             } else {
-                const user = new User({
-                    username: userName,
-                    password: passWord
-                });
-                user.save(function(){
-                    res.send("User has been registered");
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/dashboard");
                 });
             }
         }
-    });
+    );
 });
 
 app.get('/python', (req, res) => {
